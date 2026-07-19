7ef81cb635ee9982c8b95694500dff5508ca2b4c
Ferrer, Jose Antonio <jferrer9@gatech.edu>
2026-07-18 22:10:58 -0600

    Added GPL to all code files

--- frontend/src/constants/pilotThresholds.ts
+++ frontend/src/constants/pilotThresholds.ts
@@ -1,3 +1,20 @@
+/*
+ * HotSpots Campaigner - Battletech Mercenaries campaign management SaaS.
+ * Copyright (C) 2026 Jose Ferrer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // Pilot skill threshold configuration for Hot Spots & Campaigners
 // Thresholds map skill points (SP) → skill level + handicap penalty
 // Higher SP = lower skill value (better), higher handicap penalty
